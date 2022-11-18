d5d8aebd249a9702b4685d497a1e8fbeec93cbda
Jon Samp <sampjon@gmail.com>
2022-11-17 20:22:21 -0600

    adds update button

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Updates from 'expo-updates';
+import { checkForUpdateAsync } from 'expo-updates';
 
 export default function App() {
+  async function checkForUpdates() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      alert(update.isAvailable ? 'update is available' : 'no update available');
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Update" onPress={checkForUpdates} />
       <StatusBar style="auto" />
     </View>
   );
